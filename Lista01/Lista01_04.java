06fbfe2568e329bb2734e95e9aebf0a3bf6457b6
DomingosNetto <domingos.oliveira@academico.ifpb.edu.br>
2019-03-20 11:43:50 -0300

    Crie um programa java para verificar se o aluno foi aprovado(Media maior ou igual a 7,0), se vai fazer final(média maior ou igual a 5,0 e menor que 7,0) ou se foi reprovado(media abaixo de 5,0).O usuário deverá informar 4 notas.

---
+++ Lista01/Lista01_04.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Lista01_04{
+    public static void main(String[] args){
+        Scanner notas = new Scanner(System.in);
+        float N1, N2, N3, N4, media=0;
+        System.out.println("Nota 01:");
+        N1 = notas.nextFloat();
+        System.out.println("Nota 02:");
+        N2 = notas.nextFloat();
+        System.out.println("Nota 03:");
+        N3 = notas.nextFloat();
+        System.out.println("Nota 04:");
+        N4 = notas.nextFloat();
+        media = (N1+N2+N3+N4)/4;
+        System.out.println("A media do Aluno é: "+media);
+        if(media>=7.0){
+            System.out.println("APROVADO!");}
+        else if(media>=5.0 && media <7.0){
+            System.out.println("FINAL!");}
+        else if(media<5.0){
+            System.out.println("REPROVADO!");}
+}
+
+}
