09fa60e176d93b509c8a3217ccca6701d0fbc8bb
DomingosNetto <domingos.oliveira@academico.ifpb.edu.br>
2019-03-20 11:40:32 -0300

    O sistema de avaliação de determinada disciplina é composto por 3 provas. Aprimeira prova tem peso 2, a segunda tem peso 3 e a terceira tem peso 5. Faça um programa java para calcular a média final de um aluno desta disciplina

---
+++ Lista01/Lista01_03.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Lista01_03{
+    public static void main(String[] args){
+        float N1, N2, N3;
+        Scanner LerNota = new Scanner(System.in);
+        System.out.println("Nota 01:");
+        N1 = LerNota.nextFloat();
+        System.out.println("Nota 02:");
+        N2 = LerNota.nextFloat();
+        System.out.println("Nota 02:");
+        N3 = LerNota.nextFloat();
+        float media =((N1*2)+(N2*3)+(N3*5))/10;
+        System.out.println(media);
+}
+}
