76b946d9ffcd18c4edcbcf11ac9ce4ece26c537c
DomingosNetto <domingos.oliveira@academico.ifpb.edu.br>
2019-03-20 14:02:13 -0300

    Quadrados e cubos
    Desenvolva um programa java que calcule os quadrados e cubos dos números de 0 a 10 e imprima os resultados

---
+++ Lista01/Lista01_12.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Lista01_12{
+    public static void main(String[] args){
+        //Scanner ler = new Scanner(System.in);
+        int i;
+        double opr1, opr2;
+        System.out.println("Num   Quadrado   Cubo");
+        for (i=0;i<=10;i++){
+            opr1=Math.pow(i,2);
+            opr2=Math.pow(i,3);
+            System.out.println(i+"      "+opr1+"       "+opr2);
+            
+}
+}
+}
