edaa4ae3602b898554525c85266ae8988da3ad48
DomingosNetto <domingos.oliveira@academico.ifpb.edu.br>
2019-03-19 22:14:25 -0300

    Conversão de velocidade
    Crie um programa Java

---
+++ Lista01/Lista01_01.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Lista01_01{
+    public static void main(String[] args){
+        Scanner ler = new Scanner(System.in);
+        float velocidade;
+        float opr=3.6f;
+        System.out.println("Qual a velocidade em m/s?");
+        velocidade = ler.nextFloat();
+        float converssao = velocidade*opr;
+        System.out.println("Velocidade em Km/h é: "+converssao);
+}
+}
