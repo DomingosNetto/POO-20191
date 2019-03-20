14707420455af237a411bdef4ac309bb21838cdc
DomingosNetto <domingos.oliveira@academico.ifpb.edu.br>
2019-03-20 11:36:45 -0300

    Crie um programa Java que determine a idade atual da pessoa, baseado no ano de

---
+++ Lista01/Lista01_02.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Lista01_02{
+    public static void main(String[] args){
+        Scanner ler = new Scanner(System.in);
+        int anoNasc, anoAtual;
+        System.out.println("Qual seu ano de nascimento?");
+        anoNasc = ler.nextInt();
+        System.out.println("Em que ano estamos?");
+        anoAtual = ler.nextInt();
+        int idade = anoAtual-anoNasc;
+        System.out.println("Você têm "+ idade + " anos de idade");
+}
+}
